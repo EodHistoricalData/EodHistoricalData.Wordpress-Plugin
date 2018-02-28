22b487c4bb88f36e271e3fa66717d7ac2b524dc3
d@alaev.ru <suwxaizi>
2018-02-28 18:55:29 +0100

    Test

--- widget/template/ticker_widget.php
+++ widget/template/ticker_widget.php
@@ -2,11 +2,11 @@
 <?php if (!empty($title)): ?>
     <?php echo $args['before_title'] . $title . $args['after_title']; ?>
 <?php endif; ?>
-<ul class="eod_widget_ticker_list">
-<?php foreach($targetList as $i => $targetElement):  ?>
-    <li>
-    <?php echo EOD_Stock_Prices_Plugin::loadTemplate('template/ticker.php', array('tickerData' => $targetElement['tickerData'], 'target' => $targetElement['target'])); ?>
-    </li>
-<?php endforeach; ?>
-</ul>
+    <ul class="eod_widget_ticker_list">
+        <?php foreach ($targetList as $i => $targetElement): ?>
+            <li>
+                <?php echo EOD_Stock_Prices_Plugin::loadTemplate('template/ticker.php', array('tickerData' => $targetElement['tickerData'], 'target' => $targetElement['target'])); ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 <?php echo $args['after_widget']; ?>
